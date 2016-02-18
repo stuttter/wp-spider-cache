e6742c5ef1c66cf78ffa15afd6d2396c6c0f5156
bueltge <frank@bueltge.de>
2016-02-18 21:42:31 +0100

    Check, run only in admin area. Also check for fct. #7

--- drop-ins/object-cache.php
+++ drop-ins/object-cache.php
@@ -10,6 +10,10 @@
 // Exit if accessed directly
 defined( 'ABSPATH' ) || exit;
 
+if ( ! defined( 'WP_PLUGIN_DIR' ) ) {
+	define( 'WP_PLUGIN_DIR', WP_CONTENT_DIR . '/plugins' );
+} // full path, no trailing slash
+
 // Pull in required files
 require_once WP_PLUGIN_DIR . '/wp-spider-cache/includes/functions.php';
 require_once WP_PLUGIN_DIR . '/wp-spider-cache/includes/class-object-cache.php';
