486fe706d3346e7be8aeb0412fe29084f76941d3
John James Jacoby <johnjamesjacoby@me.com>
2016-10-24 14:40:44 -0500

    Bump to 4.0.0, add support for LudicrousDB cache group.

--- wp-spider-cache.php
+++ wp-spider-cache.php
@@ -7,7 +7,7 @@
  * License:     GPLv2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Description: Your friendly neighborhood caching solution for WordPress
- * Version:     3.4.0
+ * Version:     4.0.0
  * Text Domain: wp-spider-cache
  * Domain Path: /assets/lang/
  */
