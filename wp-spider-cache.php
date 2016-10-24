5d87bf830c57cd9174e97a42f9331fa6e30f39ac
John James Jacoby <johnjamesjacoby@me.com>
2016-10-24 14:41:05 -0500

    Bump asset version.

--- wp-spider-cache.php
+++ wp-spider-cache.php
@@ -38,7 +38,7 @@ class WP_Spider_Cache_UI {
 	 *
 	 * @var string
 	 */
-	private $asset_version = '201610180001';
+	private $asset_version = '201610240001';
 
 	/**
 	 * The resulting page's hook_suffix.
