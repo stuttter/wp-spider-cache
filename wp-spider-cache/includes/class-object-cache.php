84304cd725c7a3ae60e89f489326b2a497ae5971
jjj <johnjamesjacoby@me.com>
2018-10-28 10:00:41 -0500

    Add Memory cache type, to avoid breakage when Memcached isn't installed or running.

--- wp-spider-cache/includes/class-object-cache.php
+++ wp-spider-cache/includes/class-object-cache.php
@@ -15,4 +15,8 @@ require_once dirname( __FILE__ ) . '/class-object-base.php';
 // Memcached
 if ( extension_loaded( 'Memcached' ) ) {
 	require_once dirname( __FILE__ ) . '/class-object-memcached.php';
+
+// Memory
+} else {
+	require_once dirname( __FILE__ ) . '/class-object-memory.php';
 }
